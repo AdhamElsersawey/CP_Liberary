5cc89df3b9ac342f625d61753516695c0095ef71
Hazem-zmm <emamhazem278@gmail.com>
2026-08-10 18:26:26 +0300

    Update Ncr.cpp

--- Math/Ncr.cpp
+++ Math/Ncr.cpp
@@ -1,17 +1,25 @@
-struct Comp {
-    vector<Z> fact;
+ll pw(int x,int k){
+int ret=1;
+while(k){
+    if(k&1)ret=ret*x%mod;
+    x=x*x%mod;
+    k>>=1;
+}
+return ret;
+}
+int fact[N], invr[N];
 
-    Comp(int n) {
-        fact.assign(n + 5, 1);
-        for (int i = 1; i <= n; ++i)
-            fact[i] = fact[i - 1] * i;
-    }
-
-    Z nCr(int n, int r) { return n < r ? 0 : fact[n] / fact[n - r] / fact[r]; }
-
-    Z nPr(int n, int r) { return n < r ? 0 : fact[n] / fact[n - r]; }
-
-    Z catalan(int n) {
-        return fact[2 * n] / fact[n] / fact[n + 1];
-    }
-};
+void build() {
+    fact[0] = 1;
+    for(int i = 1; i < N; ++i) fact[i]=1ll*fact[i - 1]*i%mod;
+    invr[N - 1] = pw(fact[N - 1], mod - 2);
+    for(int i = N - 2; ~i; --i) invr[i] = 1ll*invr[i+1]*(i+1)%mod;
+}
+ 
+int nCr(int n, int r) {
+    if(n < r) return 0;
+    return 1ll*fact[n]*invr[n-r]%mod*invr[r]%mod;
+}
+// Z catalan(int n) {
+     //   return fact[2 * n] / fact[n] / fact[n + 1];
+    //}
